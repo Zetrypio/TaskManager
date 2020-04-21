1d1cfd4e661aac14acb349dcdbee7c0a419523d9
Zetrypio <mdemrtl@gmail.com>
2020-04-21 22:56:09 +0200

    Fixes #40 .

--- TaskManager/datedialog.py
+++ TaskManager/datedialog.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 from tkinter import *
+from tkinter.messagebox import *
 from horloge import *
 from ttkcalendar import *
 from dialog import *
 from tkinter import Label, Frame
+from util import *
 import re
 import datetime
 
@@ -16,6 +18,7 @@ def askdate(style="nombre"):
     heure = ""
     dateheure = None
     EXIT = False
+    prevHWas0 = prevMWas0 = False
     def onClose(a):
         nonlocal EXIT, date, heure, dateheure
         EXIT = True
@@ -56,8 +59,54 @@ def askdate(style="nombre"):
             m.set(mm)
             h.set(hh)
             hor.set(hh, mm)
-        except: # Il peut y avoir une exception si on ferme la fenêtre : on s'en fiche.
+        except TclError: # Il peut y avoir une exception si on ferme la fenêtre : on s'en fiche.
             pass
+        except Exception as e:
+            showerror("Symbol Interdit", err(e))
+            m.set(0)
+            h.set(0)
+            hor.set(0, 0)
+    
+    def heureChange2():
+        try:
+            print("I'm using it")
+            if prevHWas0:
+                h.delete(INSERT, END)
+            if prevMWas0:
+                m.delete(INSERT, END)
+
+            hor.setAuto(False)
+            hh = int(h.get() if h.get() else 0)
+            mm = int(m.get() if m.get() else 0)
+            
+            while mm < 0:
+                mm += 60
+                hh -= 1
+            while mm >= 60:
+                mm -= 60
+                hh += 1
+            if hh < 0:
+                hh = 0
+                mm = 0
+            if hh >= 24:
+                hh = 23
+                mm = 59
+            m.set(mm)
+            h.set(hh)
+            hor.set(hh, mm)
+        #except TclError: # Il peut y avoir une exception si on ferme la fenêtre : on s'en fiche.
+        #    pass
+        except Exception as e:
+            showerror("Symbol Interdit", err(e))
+            m.set(0)
+            h.set(0)
+            hor.set(0, 0)
+    
+    def prechange():
+        nonlocal prevHWas0, prevMWas0
+        prevHWas0 = int(h.get() if h.get() else 0) == 0
+        prevMWas0 = int(m.get() if m.get() else 0) == 0
+        print("pre", prevHWas0, prevMWas0)
 
     # Construction du dialogue :
     fen = Dialog(title = "Choix de la date et l'heure", buttons = ("Ok", "Annuler"), command=onClose)
@@ -71,20 +120,20 @@ def askdate(style="nombre"):
     hor.pack(side = TOP)
 
     # choix de l'heure :
-    v1 = Variable()
-    h = Spinbox(cadre, from_ = -1, to=24, increment = 1, width = 4, command = heureChange, textvariable = v1)
+    h = Spinbox(cadre, from_ = -1, to=24, increment = 1, width = 4, command = heureChange)
+    h.bind("<Key>", lambda e: prechange(), add = 1)
+    h.bind("<Key>", lambda e: h.after(10, heureChange2), add = 1)
     h.pack(side = LEFT, expand = YES, fill = X)
     # choix de la minute : TODO : in-/dé-crémenter les minutes ou les heures au delà de leurs périodes de définition doit augmenter les heures/ les
-    v2 = Variable()
-    m = Spinbox(cadre, from_ = -1, to=60, increment = 1, width = 4, command = heureChange, textvariable = v2)
+    m = Spinbox(cadre, from_ = -1, to=60, increment = 1, width = 4, command = heureChange)
+    m.bind("<Key>", lambda e: prechange(), add = 1)
+    m.bind("<Key>", lambda e: m.after(10, heureChange2), add = 1)
     m.pack(side = LEFT, expand = YES, fill = X)
     # valeurs de bases :
     h.set(time.localtime().tm_hour)
     m.set(time.localtime().tm_min)
 
-    v1.trace("rwua", lambda *a: heureChange())
-    v2.trace("rwua", lambda *a: heureChange())
-
+    prechange()
     # print(fen.master.master.slaves()[0].taskEditor.slaves()[1].slaves())
     # et on active le dialogue.
     fen.activateandwait()
