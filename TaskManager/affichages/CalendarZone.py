03e008cf46ceb9fa05ce3e3e85991ed9195aae3b
Squelletton <squelletton.sln@outlook.fr>
2020-05-06 17:06:16 +0200

    Decalage d'heure OK
    On peut changer décaler une ou plusieurs taches d'un nombre d'heure fixe
    + Si des taches sont hors champ, on propose d'élargir le champ
    + RadioButton pour savoir si on bloque
    + Gestion des taches sur 2 jours (enfin je crois, comme elles ne
    s'affichent pas en temps normal, pas facile de savoir)

--- TaskManager/affichages/CalendarZone.py
+++ TaskManager/affichages/CalendarZone.py
@@ -50,13 +50,22 @@ class CalendarZone(Frame):
         max2 = self.getDonneeCalendrier().getHeureFin()
         nbHeure = max2.hour - min.hour
         nb, pos = askAjouterHeure(min, max, nbHeure)
-        if nb is not None and pos is not None:
-            if   pos == "Avant":
-                min = datetime.time(min.hour - nb)
-                self.getDonneeCalendrier().setHeureDebut(min)
-            elif pos == "Apres":
-                max = datetime.time(max2.hour + nb, max2.minute)
-                self.getDonneeCalendrier().setHeureFin(max)
+        self.gestionHeure(nb, pos)
+
+    def gestionHeure(self, nombreHeure, position):
+        """
+        Fonction qui s'occupe de rajouter des heures visibles.
+        En dehors de la fonction ajouterHeure lié au bouton car on pourrait avoir à ajouter des heures autrements que par le bouton
+        @param nombreHeure : int relatif, permet d'ajouter ou retirer des heures
+        @param position    : string "Avant" / "Apres" pour savoir ou appliquer les changements
+        """
+        if nombreHeure is not None and position is not None:
+            if   position == "Avant":
+                timeHeure = datetime.time(self.getDonneeCalendrier().getHeureDebut().hour - nombreHeure)
+                self.getDonneeCalendrier().setHeureDebut(timeHeure)
+            elif position == "Apres":
+                timeHeure = datetime.time(self.getDonneeCalendrier().getHeureFin().hour + nombreHeure, self.getDonneeCalendrier().getHeureFin().minute)
+                self.getDonneeCalendrier().setHeureFin(timeHeure)
 
     def ajouterJour(self):
         totalJour = self.getDonneeCalendrier().getLongueurPeriode().days-1
@@ -68,10 +77,10 @@ class CalendarZone(Frame):
         Fonction qui s'occupe d'ajouter ou de supprimer des jours
         En dehors de la fonction ajouterJour lié au bouton car on pourrait avoir à ajouter des jours autrement que par le bouton
         @param nombreDeJour : int relatif, permet d'ajouter ou retirer des jours
-        @param position : string "Avant" / "Apres" pour savoir ou appliquer les changements
+        @param position     : string "Avant" / "Apres" pour savoir ou appliquer les changements
         """
         if nombreDeJour is not None and position is not None:
-            periode = self.getDonneeCalendrier().getPeriodeActive()
+            periode = self.getPeriodeActive()
             if   position == "Avant":
                 self.getDonneeCalendrier().setPeriodeActiveDebut(periode.getDebut() - datetime.timedelta(days=nombreDeJour))
             elif position == "Apres":
@@ -91,7 +100,7 @@ class CalendarZone(Frame):
         # Si la liste est vide on évite la question
         if len(self.getDonneeCalendrier().getSelectedTask()) == 0:
             return
-        # S'il y a une tache de plus j'un jour on est mal
+        # S'il y a une tache de plus j'un jour on est mal enfin à voir
         for tache in self.getDonneeCalendrier().getSelectedTask():
             if tache.getDuree() > datetime.timedelta(days=1):
                 showerror("Selection invalide", "Vous ne pouvez pas décaler en heure une tache de plus d'un jour.")
@@ -105,29 +114,84 @@ class CalendarZone(Frame):
             if last is None or last.getDebut() < tache.getDebut():
                 last = tache
 
-        heureRetirerMax = last.getDebut().hour - self.getDonneeCalendrier().getHeureDebut().hour
-        heureAjoutMax = self.getDonneeCalendrier().getHeureFin().hour - first.getFin().hour + 1
-        print(heureRetirerMax, heureAjoutMax)
-        nb, pos = askDecalHeure(heureRetirerMax, heureAjoutMax)
+
+        periode = self.getPeriodeActive()
+
+        heureDebut = self.getDonneeCalendrier().getHeureDebut()
+        heureFin   = self.getDonneeCalendrier().getHeureFin()
+
+        lastDiffJour = (last.getDebut().date() - periode.getDebut()).days # .days pour intifier le tout
+        firstDiffJour = (periode.getFin() - first.getFin().date()).days
+        heureRetirerMax = last.getDebut().hour + lastDiffJour * 24
+        heureAjoutMax = heureFin.hour - first.getFin().hour + 1 + firstDiffJour * 24
+        nb, pos, param = askDecalHeure(heureRetirerMax, heureAjoutMax, heureDebut, heureFin, last.getDebut().hour - heureDebut.hour, heureFin.hour - first.getFin().hour+1)
+
+        if nb is None or pos is None or param is None or nb == 0:
+            return
+
         # Ajustement des heures
+        horsChamp = False
+
         for tache in self.getDonneeCalendrier().getSelectedTask():
             # Si tout va bien
-            if  self.getDonneeCalendrier().getHeureDebut() <= (tache.getDebut()+datetime.timedelta(hours=nb)).time()\
-            and self.getDonneeCalendrier().getHeureFin()   >= (tache.getFin()+datetime.timedelta(hours=nb)).time():
-                print("ok")
+            if  (tache.getDebut()+datetime.timedelta(hours=nb)).date() == (tache.getFin()+datetime.timedelta(hours=nb)).date()\
+            and heureDebut <= (tache.getDebut()+datetime.timedelta(hours=nb)).time()\
+            and heureFin   >= (tache.getFin()+datetime.timedelta(hours=nb)).time():
+                print("140 CalZone ok")
                 tache.setDebut(tache.getDebut()+datetime.timedelta(hours=nb))
 
-            # Si on dépasse, on cadre selon les limites
-            else:
+            # Si on dépasse, on cadre selon les limites et mode bloquer
+            elif param == "bloquer":
                 # Si on retire des heures au début
                 if nb < 0:
                     # On peut pas mettre un
-                    tache.setDebut(datetime.datetime.combine(tache.getDebut().date(), self.getDonneeCalendrier().getHeureDebut()))
+                    tache.setDebut(datetime.datetime.combine(tache.getDebut().date(), heureDebut))
                 # Si on ajoute pour mettre à la fin
                 else:
-                    heureFin = self.getDonneeCalendrier().getHeureFin() # Time
+                    heureFin = heureFin # Time
                     time = datetime.datetime.combine(tache.getFin().date(), heureFin) - tache.getDuree() # datetime - timedelta
                     tache.setDebut(time)
+
+            # Si on dépasse et que l'on ne bloque pas
+            elif param == "duree":
+                tache.setDebut(tache.getDebut()+datetime.timedelta(hours=nb))
+                horsChamp = True
+
+            # Si au final il y a des taches hors champs on demande si on affiche les heures pour voir le.s tache.s
+            if horsChamp and askChangerHeure():
+                timeAvant = heureDebut
+                timeApres = heureFin
+                tacheAvant = None
+                tacheApres = None
+                for tache in self.getDonneeCalendrier().getSelectedTask():
+                    # Si le début est avant la fin (hors date) ET qu'il est avant le referent
+                    if tache.getDebut().time() < tache.getFin().time() and tache.getDebut().time() < timeAvant:
+                        tacheAvant = tache
+                        timeAvant  = tache.getDebut().time()
+
+                    # Si la fin est avant le début (hors date) ET qu'il est avant le referent
+                    elif tache.getDebut().time() > tache.getFin().time() and tache.getFin().time() < timeAvant:
+                        tacheAvant = tache
+                        timeAvant  = tache.getFin().time()
+
+                    # Si la fin est apres le debut (hors date) ET qu'il est apres le referent
+                    if tache.getDebut().time() < tache.getFin().time() and tache.getFin().time() > timeApres:
+                        tacheApres = tache
+                        timeApres  = tache.getFin().time()
+
+                    # Si le début est apres la fin (hors date) ET qu'il est apres le referent
+                    elif tache.getDebut().time() > tache.getFin().time() and tache.getDebut().time() > timeApres:
+                        tacheApres = tache
+                        timeApres  = tache.getDebut().time()
+
+
+                # Maintenant on applique les changements
+                if tacheAvant is not None:
+                    addAvant = heureDebut.hour - timeAvant.hour
+                    self.gestionHeure(addAvant, "Avant")
+                if tacheApres is not None:
+                    addApres = timeApres.hour - heureFin.hour
+                    self.gestionHeure(addApres, "Apres")
 
         self.getDonneeCalendrier().updateAffichage()
 
@@ -148,6 +212,9 @@ class CalendarZone(Frame):
     def getDonneeCalendrier(self):
         return self.zoneDynamicCalendarFrame.getDonneeCalendrier()
     
+    def getPeriodeActive(self):
+        """ Getter de la periode active """
+        return self.getDonneeCalendrier().getPeriodeActive()
     # Pour la barre d'outil des périodes :
     def voirTacheDansVue(self):
         pass
--- TaskManager/toolbar/dialog/decalageHeureDialog.py
+++ TaskManager/toolbar/dialog/decalageHeureDialog.py
@@ -6,15 +6,26 @@ import datetime
 
 from util.widgets.Dialog import *
 
-def askDecalHeure(heureRetirerMax, heureAjoutMax):
-    nbHeure = None
+def askDecalHeure(heureRetirerMax, heureAjoutMax, debut, fin, totBloque, tarBloque):
+    """
+    Dialog pour demander comment décaler les taches
+    @param heureRetirerMax : (int) nombre d'heure que l'on peut retirer maximum à la tache qui commence le plus tardivement
+    @param heureAjoutMax   : (int) nombre d'heure que l'on peut ajouter maximum à la tache qui termine le plus tot
+    @param debut           : (time) heure de début d'affichage
+    @param fin             : (time) heure de fin d'affichage
+    @param totBloque       : (int) nombre d'heure à partir duquelle le blocage devient utile
+    @param tarBloque       : (int) nombre d'heure à partir duquelle le blocage devient utile
+    """
+    nbHeure  = None
     position = None
+    param    = None
 
     def onClose(bouton):
         # Permet de modifier les valeurs des variables
-        nonlocal nbHeure, position
+        nonlocal nbHeure, position, param
         if bouton == 'Ok':
             position = varRadioGestion.get()
+            param = varRadioParam.get()
             nbHeure = int(sb.get())
             if varRadioGestion.get() == "tot":
                 nbHeure = nbHeure*-1
@@ -23,8 +34,12 @@ def askDecalHeure(heureRetirerMax, heureAjoutMax):
     # Pour adapter le nombre d'heure max du spinBoc
     def adapteSpinbox():
         newVar = 0
-        if   varRadioGestion.get() == "tot":
+        if   varRadioGestion.get() == "tot" and varRadioParam.get() == "bloquer":
+            newVar = totBloque
+        elif varRadioGestion.get() == "tot":
             newVar = heureRetirerMax
+        elif varRadioGestion.get() == "tard" and varRadioParam.get() == "bloquer":
+            newVar = tarBloque
         elif varRadioGestion.get() == "tard":
             newVar = heureAjoutMax
 
@@ -55,14 +70,30 @@ def askDecalHeure(heureRetirerMax, heureAjoutMax):
     lb.pack(side=LEFT, fill=BOTH)
     sb.pack(side=LEFT, fill=X, expand=YES)
 
+    frameParametre = Frame(fen)
+    varRadioParam = StringVar()
+    rP1 = Radiobutton(frameParametre, text = "Garder la même durée entre chaque tache", variable = varRadioParam, value = "duree", command = adapteSpinbox)
+    rP2 = Radiobutton(frameParametre, text = "Garder les taches entre %s h %s et %s h %s"%(debut.hour, debut.minute, fin.hour, fin.minute), variable =varRadioParam, value = "bloquer", command = adapteSpinbox)
+    rP1.grid(row=0, sticky="w")
+    rP2.grid(row=1, sticky="w")
+
     framePos.pack(    side = TOP, expand = YES, fill = BOTH)
     frameHeure.pack(  side = TOP, expand = YES, fill = BOTH)
-    Label(fen, text="Toutes les taches vont être décalées.\nSi c'est un nombre d'heures trop important, elles seront placées au début ou en fin de journée.").pack(side = TOP, expand = YES, fill = X)
+    frameParametre.pack(  side = TOP, expand = YES, fill = BOTH)
+
+
 
     varRadioGestion.set("tard")
+    varRadioParam.set("duree")
 
 
 
     # Active et attend (un peu comme une mainloop)
     fen.activateandwait()
-    return nbHeure, position
+    return nbHeure, position, param
+
+def askChangerHeure():
+    if askyesnowarning("Plage horaire trop court", "Une ou plusieurs taches ne sont plus visibles.\nVoulez-vous adapter la plage horaire ?"):
+        return True
+    else:
+        return False
