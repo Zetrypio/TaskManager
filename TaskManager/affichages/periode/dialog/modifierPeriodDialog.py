9beda3ebdd169f46263d53c7f4e53b909b838661
Zetrypio <mdemrtl@gmail.com>
2020-08-07 15:34:53 +0200

    Correction de modifierPeriodDialog.

--- TaskManager/affichages/periode/dialog/modifierPeriodDialog.py
+++ TaskManager/affichages/periode/dialog/modifierPeriodDialog.py
@@ -12,51 +12,32 @@ def askModifierPeriode(periodManager, taskEditor, from_ = None):
     per = from_
 
     def onClose(button):
-        if button == "Ajouter":
+        if button == "Modifier":
+            # La création d'une période permettra de récupérer ses attributs.
+            # Cependant, cette période est temporaire au calcul et n'est pas
+            # ajoutée au PeriodManager.
             periode = p.createPeriode()
             
-            ## Ajout des schedulables et autres :
-            # Il faut en faire une copie,
-            # Mais pour éviter tout les soucis de non copies en profondeur
-            # (je pense notamment aux dépendances qui pourrait alors se trouver dans une autre période...)
-            # on va faire une "sauvegarde-lecture" dans la RAM.
-            # TODO : FIXME
-            data = per.saveByDict()
-            periodManager.supprimer(per)
-            per = Periode.load(data, periodManager)
             # Puis on remet les attributs :
             per.setNom(periode.getNom())
             per.setDebut(periode.getDebut())
             per.setFin(periode.getFin())
             per.setDescription(periode.getDescription())
             per.setColor(periode.getColor())
-            periodManager.ajouter(per)
-    
-    def supprimerPuisAjouter(*a):
-        pass # TODO quand on aura de quoi faire.
-#        taskEditor.remove(per)
-#        taskEditor.ajouter(*a)
-
-    def configurer():
-        periodManager.supprimerPeriodes()
-        valider()
     
     fen = Dialog(command = onClose,
                  buttons = ("Modifier", "Annuler"),
                  exitButton = ("Modifier", "Annuler", "Fermer", "WM_DELETE_WINDOW"))
 
-    fen.redessiner = taskEditor.redessiner
-    fen.ajouter = taskEditor.ajouter
+    # Pour faire croire au PeriodAdder que le Dialog est le TaskEditor.
+    fen.redessiner     = taskEditor.redessiner
+    fen.ajouter        = taskEditor.ajouter
+    fen.getApplication = taskEditor.getApplication
     
     p = PeriodAdder(periodManager, fen)
     p.pack(expand = YES, fill = BOTH)
     p.boutonValider.grid_forget()
     
-#    # Sauvegarder les tâches : TODO
-#    if per:
-#        listSchedulables =      per.listSchedulables
-#        listAllThingsInPeriod = per.listAllThingsInPeriod
-
     # Ajouter les champs déjà rempli si on a une période :
     if per is not None:
         p.debut = per.getDebut()
