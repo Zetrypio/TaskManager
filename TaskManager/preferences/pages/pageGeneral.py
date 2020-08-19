c2475e2ba1c3c8959084b7eb9335082853cda1ec
Squelletton <squelletton.sln@outlook.fr>
2020-08-19 20:49:15 +0200

    widget du choix de l'auto save

--- TaskManager/preferences/pages/pageGeneral.py
+++ TaskManager/preferences/pages/pageGeneral.py
@@ -15,27 +15,58 @@ class PageGeneral(AbstractPage):
         # Note : Si on rajoute une option, ne pas oublier d'ajouter la variable de contrôle à self._listData.append([variable, "texte explicatif", valeurParDefaut])
         # Note : Si l'option que l'on souhaite ajouter nécéssite un redémarrage pour s'appliquer, utiliser la méthode "self._addDataNeedRestart(liste)", avec la même liste que pour self._listData
 
+        ## Variable
+        self.__varCaseTypeHorloge = BooleanVar()
+        self._listData.append([self.__varCaseTypeHorloge, "Afficher les heures sur l'horloge", True])
+        self.__varChargerLastPeriode = BooleanVar()
+        self._addDataNeedRestart([self.__varChargerLastPeriode, "charger dernière période", False])
+        self.__varAllowAutoSave = BooleanVar()
+        self._addDataNeedRestart([self.__varAllowAutoSave, "auto-save", True])
+        self.__varNbAutoSave = StringVar()
+        self._listData.append([self.__varNbAutoSave, "intervalle auto-save", 5])
+        self.__varUnitAutoSave = StringVar()
+        self._listData.append([self.__varUnitAutoSave, "unité de l'interval", "minutes"])
+
         ## LabelFrame Horloge
         self.__horlogeLabelFrame = LabelFrame(self._mFrame, text="Horloge")
         # widgets
-        self.__varCaseTypeHorloge = BooleanVar()
-        self._listData.append([self.__varCaseTypeHorloge, "Afficher les heures sur l'horloge", True])
         self.__caseTypeHorloge = Checkbutton(self.__horlogeLabelFrame, text = "Afficher les heures sur l'horloge lors de la sélection de la date d'une tache", variable=self.__varCaseTypeHorloge)
+        ## LabelFrame Sauvegarde & Chargement
+        self.__saveAndLoad = LabelFrame(self._mFrame, text = "Chargement et sauvegarde")
+        self.__caseChargerPeriode = Checkbutton(self.__saveAndLoad, text = "Charger la dernière période utilisé", variable = self.__varChargerLastPeriode)
+        self.__caseAllowAutoSave = Checkbutton(self.__saveAndLoad, text = "Sauvegarde automatique", variable = self.__varAllowAutoSave, command = self.__allowChangeAutosave)
+        self.__sbIntervalle = Spinbox(self.__saveAndLoad, from_ = 1, to = 100, textvariable = self.__varNbAutoSave)
+        self.__cbUnitAutoSave = Combobox(self.__saveAndLoad, value = ["heures", "minutes"], textvariable = self.__varUnitAutoSave)
 
-        self.__varChargerLastPeriode = BooleanVar()
-        self._addDataNeedRestart([self.__varChargerLastPeriode, "charger dernière période", False])
-        self.__caseChargerPeriode = Checkbutton(self._mFrame, text = "Charger la dernière période utilisé", variable = self.__varChargerLastPeriode)
 
+        ## Affichage
+        self.__horlogeLabelFrame.grid( row = 0, column = 0, sticky="NWES")
+        self.__caseTypeHorloge.pack(   side = TOP, expand = NO, fill = X)
+        self.__saveAndLoad.grid(       row = 1, column = 0)
 
-        # Affichage
-        self.__horlogeLabelFrame.grid(column = 0, row = 0, sticky="NWES")
-        self.__caseTypeHorloge.pack(side = TOP, expand = NO, fill = X)
-        self.__caseChargerPeriode.grid(row = 1, column = 0, sticky = "nsew")
+        self.__caseChargerPeriode.grid(row = 0, column = 0, sticky = "nsew")
+        self.__caseAllowAutoSave.grid( row = 1, column = 0, sticky = "we")
+        self.__sbIntervalle.grid(      row = 1, column = 1)
+        self.__cbUnitAutoSave.grid(    row = 1, column = 2)
 
         # Initialisation
         self._loadDataFile()
+        self.__allowChangeAutosave()
 
     "" # Marque pour le repli de code
+    ##############
+    # Méthodes : #
+    ##############
+    ""
+    def __allowChangeAutosave(self):
+        """
+        Méthode qui gère l'état des widget de l'intervelle de l'autoSave
+        """
+        mode = "normal" if self.__varAllowAutoSave else "disabled"
+        self.__sbIntervalle.config( state = mode)
+        self.__cbUnitAutoSave.config(state = mode)
+
+    ""
     ###################################
     # Méthodes liées à la fermeture : #
     ###################################
