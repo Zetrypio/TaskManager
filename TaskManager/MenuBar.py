7a6c13c0fb3e4e5bf355b50dfc013c406a6a6f96
Zetrypio <mdemrtl@gmail.com>
2020-08-19 21:01:47 +0200

    Ajout du menu dans la MenuBar pour undo/redo.

--- TaskManager/MenuBar.py
+++ TaskManager/MenuBar.py
@@ -5,6 +5,8 @@ from tkinter import Label, Frame
 
 from preferences.fenetrePreferences import *
 
+from util.UndoRedo import *
+
 class MenuBar(Menu):
     """
     Classe de la barre de menus.
@@ -23,9 +25,7 @@ class MenuBar(Menu):
         self.menuAffichage = Menu(self, tearoff=0)
         
         self.add_cascade(label = "Fichier", menu=self.menuFichier)
-        # Il y a juste à décommenter cette ligne pour remettre le menu édition,
-        # mais tant qu'il n'y a rien dedans, on s'en fiche...
-        #self.add_cascade(label = "Edition", menu=self.menuEdition)
+        self.add_cascade(label = "Edition", menu=self.menuEdition)
         self.add_cascade(label = "Affichage", menu=self.menuAffichage)
 
         # Menu Fichier :
@@ -33,6 +33,10 @@ class MenuBar(Menu):
         self.menuFichier.add_separator()
         self.menuFichier.add_command(label = "Restart",     accelerator=self.__getBindingOf("restart"),   command = master.restart)
         self.menuFichier.add_command(label = "Quitter",     accelerator=self.__getBindingOf("quit"),      command = master.quit)
+
+        # Menu Edition :
+        self.menuEdition.add_command(label = "Annuler",  accelerator=self.__getBindingOf("undo"), command = UndoRedo.undo)
+        self.menuEdition.add_command(label = "Rétablir", accelerator=self.__getBindingOf("redo"), command = UndoRedo.redo)
 
         # Menu Affichage :
         self.menuAffichage.add_command(label = "Préférences", accelerator=self.__getBindingOf("preferences"), command = master.preferences)
@@ -42,6 +46,9 @@ class MenuBar(Menu):
         @param bindingVirtuel : <str> nom du bindings dont on cherche la combinaison
         @return                 <str> les combinaisons séparées par une virgule
         """
-        return ",".join("+".join(i.capitalize() for i in j.strip("<>").split("-")) for j in self.master.getBindingIn("Application")[bindingVirtuel]["bindings"])
+        try:
+            return ",".join("+".join(i.capitalize() for i in j.strip("<>").split("-")) for j in self.master.getBindingIn("Application")[bindingVirtuel]["bindings"])
+        except:
+            return None
 
 
