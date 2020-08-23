ad791356054a37dc677e801789d2f1ec4cb2d4f5
Squelletton <squelletton.sln@outlook.fr>
2020-08-23 11:48:32 +0200

    attributs privee dans TaskParametre.py

--- TaskManager/schedulable/task/dialog/TaskParametre.py
+++ TaskManager/schedulable/task/dialog/TaskParametre.py
@@ -36,82 +36,82 @@ class TaskParametre(AbstractSchedulableParametre):
         #varCouleur = StringVar()
         # textDesc (je le met ici pour ne pas l'oublier)"""
         if not self._getSchedulable().isContainer():
-            self.varDebut = "" # Initialisation, affectation après
-            self.varFin = ""
-            self.varDuree = None
-            self.varJour = IntVar()
-            self.varHour = IntVar()
-            self.varMin = IntVar()
-        self.varNbRep = IntVar()
-        self.varRepTimedelta = None
-        self.varRep = IntVar()
-        self.varUnitRep = StringVar()
-        self.varDone = BooleanVar()
-        self.__varGroupe = StringVar()
+            self.__varDebut    = "" # Initialisation, affectation après
+            self.__varFin      = ""
+            self.__varDuree    = None
+            self.__varJour     = IntVar()
+            self.__varHour     = IntVar()
+            self.__varMin      = IntVar()
+        self.__varNbRep        = IntVar()
+        self.__varRepTimedelta = None
+        self.__varRep          = IntVar()
+        self.__varUnitRep      = StringVar()
+        self.__varDone         = BooleanVar()
+        self.__varGroupe       = StringVar()
 
 
         # Affectation des variables
         if not self._getSchedulable().isContainer():
-            self.varDebut = self._getSchedulable().getDebut()
-            self.varFin = self._getSchedulable().getFin()
-            self.varDuree = self._getSchedulable().getDuree()
-            self.varJour.set(self.varDuree.days)
-            self.varHour.set(self.varDuree.seconds//3600)
-            self.varMin.set(self.varDuree.seconds//60%60)
-        self.varNbRep.set(self._getSchedulable().getNbRep())
-        self.varRepTimedelta = self._getSchedulable().getRep() if self._getSchedulable().getRep() is not None else datetime.timedelta()
-        self.varRep = self.varRepTimedelta.seconds//3600 if self.varRepTimedelta.days == 0 else self.varRepTimedelta.days
-        self.varDone.set(self._getSchedulable().isDone())
-        if self.varRep == 0: # ici les heures valent 0
-            self.varUnitRep.set("jours")
-        elif self.varRepTimedelta.days == 0 and self.varRep != 0:
-            self.varUnitRep.set("heures")
-        elif self.varRepTimedelta.days % 7 == 0:
-            self.varUnitRep.set("semaines")
+            self.__varDebut    = self._getSchedulable().getDebut()
+            self.__varFin      = self._getSchedulable().getFin()
+            self.__varDuree    = self._getSchedulable().getDuree()
+            self.__varJour.set(  self.__varDuree.days)
+            self.__varHour.set(  self.__varDuree.seconds//3600)
+            self.__varMin.set(   self.__varDuree.seconds//60%60)
+        self.__varNbRep.set(     self._getSchedulable().getNbRep())
+        self.__varRepTimedelta = self._getSchedulable().getRep() if self._getSchedulable().getRep() is not None else datetime.timedelta()
+        self.__varRep          = self.varRepTimedelta.seconds//3600 if self.__varRepTimedelta.days == 0 else self.__varRepTimedelta.days
+        self.__varDone.set(      self._getSchedulable().isDone())
+        if self.__varRep == 0: # ici les heures valent 0
+            self.__varUnitRep.set("jours")
+        elif self.__varRepTimedelta.days == 0 and self.__varRep != 0:
+            self.__varUnitRep.set("heures")
+        elif self.__varRepTimedelta.days % 7 == 0:
+            self.__varUnitRep.set("semaines")
         else:
-            self.varUnitRep.set("jours")
-        self.__varGroupe.set(str(self._getSchedulable().getGroupe()) + "   ID : " +self._getSchedulable().getGroupe().getUniqueID()) if self._getSchedulable().hasGroupe() else self.__varGroupe.set("N'est pas dans un groupe")
+            self.__varUnitRep.set("jours")
+        self.__varGroupe.set(    str(self._getSchedulable().getGroupe()) + "   ID : " +self._getSchedulable().getGroupe().getUniqueID()) if self._getSchedulable().hasGroupe() else self.__varGroupe.set("N'est pas dans un groupe")
 
 
         ## Attributs généraux
         if not self._getSchedulable().isContainer():
-            self.lbDebut = Label(           self._frameGeneral, text = "Début :")
-            self.btnDebut = Button(         self._frameGeneral, text = self.varDebut, command = self.__askDebut)
-            self.lbFin = Label(             self._frameGeneral, text = "Fin :")
-            self.btnFin = Button(           self._frameGeneral, text = self.varFin, command = self.__askFin)
-            self.lbDuree = Label(           self._frameGeneral, text = "Durée :")
+            self.__lbDebut =    Label( self._frameGeneral, text = "Début :")
+            self.__btnDebut =   Button(self._frameGeneral, text = self.__varDebut, command = self.__askDebut)
+            self.__lbFin =      Label( self._frameGeneral, text = "Fin :"  )
+            self.__btnFin =     Button(self._frameGeneral, text = self.__varFin,   command = self.__askFin)
+            self.__lbDuree =    Label( self._frameGeneral, text = "Durée :")
             # Duree
-            self.frameDuree = Frame(        self._frameGeneral)
-            self.sbJour = Spinbox(          self.frameDuree, from_ = 0, to=31, increment = 1, width = 4, command = self.__autoSetFin, textvariable = self.varJour)
-            self.lbJour = Label(            self.frameDuree, text = "jours")
-            self.sbHour = Spinbox(          self.frameDuree, from_ = 0, to=23, increment = 1, width = 4, command = self.__autoSetFin, textvariable = self.varHour)
-            self.lbHour = Label(            self.frameDuree, text = "heures")
-            self.sbMin = Spinbox(           self.frameDuree, from_ = 0, to=59, increment = 1, width = 4, command = self.__autoSetFin, textvariable = self.varMin)
-            self.lbMin= Label(              self.frameDuree, text = "minutes")
+            self.__frameDuree = Frame(    self._frameGeneral)
+            self.__sbJour =     Spinbox(  self.__frameDuree, from_ = 0, to=31, increment = 1, width = 4, command = self.__autoSetFin, textvariable = self.__varJour)
+            self.__lbJour =     Label(    self.__frameDuree, text = "jours")
+            self.__sbHour =     Spinbox(  self.__frameDuree, from_ = 0, to=23, increment = 1, width = 4, command = self.__autoSetFin, textvariable = self.__varHour)
+            self.__lbHour =     Label(    self.__frameDuree, text = "heures")
+            self.__sbMin =      Spinbox(  self.__frameDuree, from_ = 0, to=59, increment = 1, width = 4, command = self.__autoSetFin, textvariable = self.__varMin)
+            self.__lbMin =      Label(    self.__frameDuree, text = "minutes")
         # Répétition
-        self.lbNbRep = Label(           self._frameGeneral, text = "Nombre de répétitions :")
-        self.sbNbRep = Spinbox(         self._frameGeneral, from_ = -1, to = 100, increment = 1, width = 4, textvariable = self.varNbRep)
-        self.lbRepet = Label(           self._frameGeneral, text = "Fréquence :")
-        self.frameRepet = Frame(        self._frameGeneral)
-        self.sbRep = Spinbox(           self.frameRepet, from_ = 1, to = 100, increment = 1, width = 4, textvariable = self.varRep)
-        self.cbUnit = Combobox(         self.frameRepet, value = ["semaine", "jours", "heures"], textvariable = self.varUnitRep)
+        self.__lbNbRep =    Label(   self._frameGeneral, text = "Nombre de répétitions :")
+        self.__sbNbRep =    Spinbox( self._frameGeneral, from_ = -1, to = 100, increment = 1, width = 4, textvariable = self.__varNbRep)
+        self.__lbRepet =    Label(   self._frameGeneral, text = "Fréquence :")
+        self.__frameRepet = Frame(   self._frameGeneral)
+        self.__sbRep =      Spinbox( self.__frameRepet, from_ = 1, to = 100, increment = 1, width = 4, textvariable = self.__varRep)
+        self.__cbUnit =     Combobox(self.__frameRepet, value = ["semaine", "jours", "heures"], textvariable = self.__varUnitRep)
 
         ## Attributs avancés
         # (voir parent) self._frameAdvanced = LabelFrame(self._pageAvancee, text = "Options avancées")
-        self.lbSubtask = Label(      self._frameAdvanced, text = "Sous-tâches :")
-        self.lbListSub = Label(      self._frameAdvanced, text = self.__getListTask(self._getSchedulable().getSubTasks()) if self._getSchedulable().isContainer() else "Tache non conteneur", anchor = "nw")
-        self.lbDepces = Label(       self._frameAdvanced, text = "Dépendances :")
-        self.lbListDepces = Listbox( self._frameAdvanced, listvariable = self.__getListTask(self._getSchedulable().getDependances(), StringV = True), selectmode = "single", height = len(self._getSchedulable().getDependances()) if len(self._getSchedulable().getDependances()) > 0 else 1 )
-        self.btnSupprDepces = Button(self._frameAdvanced, text = "Supprimer", command = lambda : self.__supprimeLien("depces"))
-        self.lbDeptes = Label(       self._frameAdvanced, text = "Dépendantes :")
-        self.lbListDeptes = Listbox( self._frameAdvanced, listvariable = self.__getListTask(self._getSchedulable().getDependantes(), StringV = True), selectmode = "single", height = len(self._getSchedulable().getDependantes()) if len(self._getSchedulable().getDependantes()) > 0 else 1 )
-        self.btnSupprDeptes = Button(self._frameAdvanced, text = "Supprimer", command = lambda : self.__supprimeLien("deptes"))
-        self.lbDone = Label(         self._frameAdvanced, text = "Fait :")
-        self.cbDone = Checkbutton(   self._frameAdvanced, variable = self.varDone)
-        self.lbParent = Label(       self._frameAdvanced, text = "Parent :")
-        self.lbResultParent = Label( self._frameAdvanced, text = self._getSchedulable().getParent() if self._getSchedulable().getParent() else "")
-        self.__lbGroupe = Label(self._frameAdvanced, text = "Groupe :")
-        self.__lbResultGroupe = Label(self._frameAdvanced, text = self.__varGroupe.get())
+        self.__lbSubtask =      Label(      self._frameAdvanced, text = "Sous-tâches :")
+        self.__lbListSub =      Label(      self._frameAdvanced, text = self.__getListTask(self._getSchedulable().getSubTasks()) if self._getSchedulable().isContainer() else "Tache non conteneur", anchor = "nw")
+        self.__lbDepces =       Label(      self._frameAdvanced, text = "Dépendances :")
+        self.__lbListDepces =   Listbox(    self._frameAdvanced, listvariable = self.__getListTask(self._getSchedulable().getDependances(), StringV = True), selectmode = "single", height = len(self._getSchedulable().getDependances()) if len(self._getSchedulable().getDependances()) > 0 else 1 )
+        self.__btnSupprDepces = Button(     self._frameAdvanced, text = "Supprimer", command = lambda : self.__supprimeLien("depces"))
+        self.__lbDeptes =       Label(      self._frameAdvanced, text = "Dépendantes :")
+        self.__lbListDeptes =   Listbox(    self._frameAdvanced, listvariable = self.__getListTask(self._getSchedulable().getDependantes(), StringV = True), selectmode = "single", height = len(self._getSchedulable().getDependantes()) if len(self._getSchedulable().getDependantes()) > 0 else 1 )
+        self.__btnSupprDeptes = Button(     self._frameAdvanced, text = "Supprimer", command = lambda : self.__supprimeLien("deptes"))
+        self.__lbDone =         Label(      self._frameAdvanced, text = "Fait :")
+        self.__cbDone =         Checkbutton(self._frameAdvanced, variable = self.__varDone)
+        self.__lbParent =       Label(      self._frameAdvanced, text = "Parent :")
+        self.__lbResultParent = Label(      self._frameAdvanced, text = self._getSchedulable().getParent() if self._getSchedulable().getParent() else "")
+        self.__lbGroupe =       Label(      self._frameAdvanced, text = "Groupe :")
+        self.__lbResultGroupe = Label(      self._frameAdvanced, text = self.__varGroupe.get())
 
 
 
@@ -120,40 +120,40 @@ class TaskParametre(AbstractSchedulableParametre):
         # Général
         # grid obligatoire
         if not self._getSchedulable().isContainer():
-            self.lbDebut.grid(     row = 6,  column = 0, sticky = "e" )
-            self.btnDebut.grid(    row = 6,  column = 1, sticky = "we")
-            self.lbFin.grid(       row = 7,  column = 0, sticky = "e" )
-            self.btnFin.grid(      row = 7,  column = 1, sticky = "we")
+            self.__lbDebut.grid(     row = 6,  column = 0, sticky = "e" )
+            self.__btnDebut.grid(    row = 6,  column = 1, sticky = "we")
+            self.__lbFin.grid(       row = 7,  column = 0, sticky = "e" )
+            self.__btnFin.grid(      row = 7,  column = 1, sticky = "we")
             # Duree
-            self.lbDuree.grid(     row = 8,  column = 0, sticky = "e" )
-            self.frameDuree.grid(  row = 8,  column = 1, sticky = "we")
-            self.sbJour.grid(      row = 0,  column = 0)
-            self.lbJour.grid(      row = 0,  column = 1)
-            self.sbHour.grid(      row = 0,  column = 2)
-            self.lbHour.grid(      row = 0,  column = 3)
-            self.sbMin.grid(       row = 0,  column = 4)
-            self.lbMin.grid(       row = 0,  column = 5)
-        self.lbNbRep.grid(         row = 9,  column = 0, sticky = "e" )
-        self.sbNbRep.grid(         row = 9,  column = 1, sticky = "w")
-        self.frameRepet.grid(      row = 10, column = 1, sticky = "we")
-        self.sbRep.pack( side = LEFT, fill = BOTH)
-        self.cbUnit.pack(side = LEFT, fill = BOTH, expand = YES)
+            self.__lbDuree.grid(     row = 8,  column = 0, sticky = "e" )
+            self.__frameDuree.grid(  row = 8,  column = 1, sticky = "we")
+            self.__sbJour.grid(      row = 0,  column = 0)
+            self.__lbJour.grid(      row = 0,  column = 1)
+            self.__sbHour.grid(      row = 0,  column = 2)
+            self.__lbHour.grid(      row = 0,  column = 3)
+            self.__sbMin.grid(       row = 0,  column = 4)
+            self.__lbMin.grid(       row = 0,  column = 5)
+        self.__lbNbRep.grid(         row = 9,  column = 0, sticky = "e" )
+        self.__sbNbRep.grid(         row = 9,  column = 1, sticky = "w")
+        self.__frameRepet.grid(      row = 10, column = 1, sticky = "we")
+        self.__sbRep.pack( side = LEFT, fill = BOTH)
+        self.__cbUnit.pack(side = LEFT, fill = BOTH, expand = YES)
         # Avancée
         # (voir parent) self._frameAdvanced.pack(side = TOP, fill = BOTH, expand = YES)
-        self.lbSubtask.grid(     row = 1, column = 0, sticky = "e" )
-        self.lbListSub.grid(     row = 1, column = 1, sticky = "w" )
-        self.lbDepces.grid(      row = 2, column = 0, sticky = "ne")
-        self.lbListDepces.grid(  row = 2, column = 1, sticky = "nswe", rowspan = 2)
-        self.btnSupprDepces.grid(row = 3, column = 0, sticky = "e" )
-        self.lbDeptes.grid(      row = 4, column = 0, sticky = "ne")
-        self.lbListDeptes.grid(  row = 4, column = 1, sticky = "nswe", rowspan = 2)
-        self.btnSupprDeptes.grid(row = 5, column = 0, sticky = "e" )
-        self.lbDone.grid(        row = 6, column = 0, sticky = "e" )
-        self.cbDone.grid(        row = 6, column = 1, sticky = "we")
-        self.lbParent.grid(      row = 7, column = 0, sticky = "e" )
-        self.lbResultParent.grid(row = 7, column = 1, sticky = "w" )
-        self.__lbGroupe.grid(row = 8, column = 0, sticky = "e")
-        self.__lbResultGroupe.grid(row = 8, column = 1, sticky = "w")
+        self.__lbSubtask.grid(       row = 1, column = 0, sticky = "e" )
+        self.__lbListSub.grid(       row = 1, column = 1, sticky = "w" )
+        self.__lbDepces.grid(        row = 2, column = 0, sticky = "ne")
+        self.__lbListDepces.grid(    row = 2, column = 1, sticky = "nswe", rowspan = 2)
+        self.__btnSupprDepces.grid(  row = 3, column = 0, sticky = "e" )
+        self.__lbDeptes.grid(        row = 4, column = 0, sticky = "ne")
+        self.__lbListDeptes.grid(    row = 4, column = 1, sticky = "nswe", rowspan = 2)
+        self.__btnSupprDeptes.grid(  row = 5, column = 0, sticky = "e" )
+        self.__lbDone.grid(          row = 6, column = 0, sticky = "e" )
+        self.__cbDone.grid(          row = 6, column = 1, sticky = "we")
+        self.__lbParent.grid(        row = 7, column = 0, sticky = "e" )
+        self.__lbResultParent.grid(  row = 7, column = 1, sticky = "w" )
+        self.__lbGroupe.grid(      row = 8, column = 0, sticky = "e" )
+        self.__lbResultGroupe.grid(row = 8, column = 1, sticky = "w" )
 
         # Config grid
         self._frameAdvanced.columnconfigure(1, weight = 1)
@@ -167,10 +167,10 @@ class TaskParametre(AbstractSchedulableParametre):
         # Géré par le parent : nom, période, couleur, desc
         super().onClose()
         if not self._getSchedulable().isContainer():
-            self._getSchedulable().setDebut(         self.varDebut, change = "duree")
-            self._getSchedulable().setDuree(datetime.timedelta(days = self.varJour.get(), hours = self.varHour.get(), minutes = self.varMin.get()))
+            self._getSchedulable().setDebut(self.__varDebut, change = "duree")
+            self._getSchedulable().setDuree(datetime.timedelta(days = self.__varJour.get(), hours = self.__varHour.get(), minutes = self.__varMin.get()))
         #self._getSchedulable().setRep # TODO
-        self._getSchedulable().setDone(          self.varDone.get())
+        self._getSchedulable().setDone(self.__varDone.get())
 
         self._getSchedulable().getApplication().getTaskEditor().redessiner()
         self._getSchedulable().getApplication().getDonneeCalendrier().updateAffichage(True) # Le True pour les liens de l'issue 92
