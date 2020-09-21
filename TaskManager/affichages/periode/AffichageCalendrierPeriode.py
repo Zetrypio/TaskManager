02ffbf7e48d3a1c77d458ded27aefe0fe03e1f10
Zetrypio <mdemrtl@gmail.com>
2020-09-21 19:23:37 +0200

    Ajout de commentaires.

--- TaskManager/affichages/periode/AffichageCalendrierPeriode.py
+++ TaskManager/affichages/periode/AffichageCalendrierPeriode.py
@@ -183,13 +183,19 @@ class AffichageCalendrierPeriode(AbstractDisplayedCalendar):
         nbJour = (dernierJour - premierJour).days
         dernierJour = datetime.date(year = self.annee, month = self.mois, day = nbJour)
 
+        # On initialise l'affichage :
         self.can.delete(ALL)
         self.__listeHauteur = {}
+
+        # Avec des variables de taille d'affichage :
         hh = 20
         w = self.can.winfo_width()
         h = self.can.winfo_height()
         nbSemaine = getNumberOfWeek()
+
+        # Bandeau des jours :
         self.can.create_rectangle(0, 0, w, hh, fill = self.getPalette()["highlightedWidget"]) # old : "light gray"
+
         # Le carré du jour actuel
         if self.mois == time.localtime().tm_mon and self.annee == time.localtime().tm_year:
             today = datetime.datetime.today()
@@ -197,25 +203,35 @@ class AffichageCalendrierPeriode(AbstractDisplayedCalendar):
             ligne = today.isocalendar()[1] - premierJour.isocalendar()[1] if premierJour.isocalendar()[1] < dernierJour.isocalendar()[1] else today.isocalendar()[1]
             y = int(hh + (h-hh)*ligne/nbSemaine*1)+1
             self.can.create_rectangle(x, y, x+w/7, y + (h-hh)/nbSemaine, fill = self.getPalette()["jour"], width = 0)
+        
+        # Le bandeau avec le nom des jours :
         for i, j in enumerate(JOUR):
             self.can.create_text(int(i*w/7+w/14), int(hh/2), width = w, text = j, fill = self.getPalette()["foreground"])
+            
+            # Lignes verticales du calendrier :
             self.can.create_line(int(i*w/7), hh+1 if premierJour.weekday()<i+1 else hh+(h-hh)/nbSemaine, int(i*w/7), h if dernierJour.weekday()+1>i-1 else h-(h-hh)/nbSemaine , fill = self.getPalette()["foreground"]) # old : "light gray"
 
+        # Lignes horizontales du calendrier :
         for i in range(nbSemaine):
             self.can.create_line(0, hh+(h-hh)/nbSemaine*(i+1), w, hh+(h-hh)/nbSemaine*(i+1), fill = self.getPalette()["foreground"])
+
+        # Numéro du jour dans la case du calendrier :
         jour = self.getJourDebut()
         semaine = 1
-        while jour.month == self.mois:
+        while jour.month == self.mois: # TODO : Utiliser Calendar.itermonthdates() ?
             self.can.create_text(int(jour.weekday())*w/7+5, semaine*(h-hh)/nbSemaine+hh, anchor = "sw", text = jour.day, fill = self.getPalette()["foreground"])
             if jour.isoweekday()%7 == 0:
                 semaine += 1
             jour += datetime.timedelta(days = 1)
         
         ############################
-        # Affichage des périodes : #
+        # Affichage des périodes : #    # Une autre fonction pour séparer le code ? Non nécessaire, mais pourquoi pas.
         ############################
         
+        # Cet ID sera unique pour chaque groupe de rectangle représentant une même période.
         NEW_TAG_ID = 0
+
+        # On parcours les périodes :
         for p in self.getApplication().getPeriodManager().getPeriodes():
             if (p.getDebut().month != self.mois and p.getFin().month != self.mois) or p.getDebut().year != self.annee:
                 continue
